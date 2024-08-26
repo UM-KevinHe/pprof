27ce6a86f762544be2ddf59b8841add4fa88a678
Xiaohan Liu <xhliuu@umich.edu>
2024-08-25 22:46:20 -0400

    update

--- R/linear_fe.R
+++ R/linear_fe.R
@@ -9,6 +9,151 @@
 #'
 #' @useDynLib pprof, .registration = TRUE
 
+
+linear_fe <- function(Y, Z, ID, Rcpp = TRUE){
+  if (missing(Y) || missing(Z) || missing(ID))
+    stop("Arguments 'Y', 'Z', and 'ID' are all required!", call.=FALSE)
+
+  if (length(Y) != length(ID) | (length(ID) != nrow(Z))){
+    stop("Dimensions of the input data do not match!!", call.=F)
+  }
+
+  data <- as.data.frame(cbind(Y, ID, Z))
+  Y.char <- colnames(data)[1]
+  prov.char <- colnames(data)[2]
+  Z.char <- colnames(Z)
+  data <- data[order(factor(data[,prov.char])),]
+
+  n.prov <- sapply(split(data[, Y.char], data[, prov.char]), length)
+  m <- length(n.prov) # number of providers
+  n <- sum(n.prov) # number of observations
+  p <- length(Z.char) # number of covariates
+
+  Z <- as.matrix(data[,Z.char], drop = F)
+  Y <- as.matrix(data[, Y.char, drop = F])
+  ID <- as.matrix(data[, prov.char, drop = F])
+
+  if (Rcpp == F) {
+    Q <- lapply(n.prov, function(n) diag(n)-matrix(1, nrow = n, ncol = n)/n)
+
+
+    # Coefficients
+    beta <- matrix(solve(t(Z)%*%bdiag(Q)%*%Z)%*%t(Z)%*%bdiag(Q)%*%Y, ncol = 1)
+    colnames(beta) <- "beta"
+    rownames(beta) <- Z.char
+
+    y_bar <- sapply(split(data[,Y.char], data[,prov.char]),mean)
+    Z_bar <- t(matrix(sapply(split(data[,Z.char, drop = FALSE], data[,prov.char]), colMeans),
+                      ncol=length(y_bar), nrow = length(beta)))
+    gamma.prov <- as.matrix(y_bar - Z_bar %*% beta)
+    colnames(gamma.prov) <- "gamma"
+    rownames(gamma.prov)<- names(n.prov)
+
+    coefficient <- list()
+    coefficient$beta <- beta
+    coefficient$gamma <- gamma.prov
+
+    # Prediction
+    linear_pred <- Z %*% beta
+    gamma.obs <- rep(gamma.prov, n.prov)
+    pred = gamma.obs + linear_pred
+    residuals <- Y - pred
+    sigma_hat_sq <- sum(residuals^2)/(n - m - p)
+
+    # Variance
+    varcor_beta <- matrix(sigma_hat_sq * solve(t(Z)%*%bdiag(Q)%*%Z), ncol = p, nrow = p)
+    rownames(varcor_beta) <- Z.char
+    colnames(varcor_beta) <- Z.char
+
+    var_gamma <- matrix(sigma_hat_sq/n.prov, ncol = 1)
+    rownames(var_gamma) <- names(n.prov)
+    colnames(var_gamma) <- "Variance.Gamma"
+
+    char_list <- list(Y.char = Y.char,
+                      prov.char = prov.char,
+                      Z.char = Z.char)
+
+    result <- structure(list(coefficient = coefficient,
+                             linear_pred = linear_pred,
+                             prediction = pred,
+                             observation = Y,
+                             prov = data[, prov.char]),
+                        class = "linear_fe")  #define a list for prediction
+
+    result$data_include <- data
+    result$char_list <- char_list
+  }
+
+  if (Rcpp == T) {
+    Y <- as.vector(Y)
+    ID <- as.vector(ID)
+    ls <- compute_profilkd_linear(Y, Z, ID, n.prov)
+    gamma.prov <- as.numeric(ls$gamma); beta <- as.numeric(ls$beta)
+    return_ls$coefficient$gamma <- gamma.prov
+    return_ls$coefficient$beta <- beta
+  }
+
+  return(result)
+}
+
+
+
+linear_fe.fit <- function(formula, data, ID.char, message = TRUE){
+  if (missing(formula) || missing(data))
+    stop("Arguments 'formula' and 'data' are required!", call.=FALSE)
+
+  if (!inherits(formula, "formula"))
+    stop("Argument 'formula' must be a formula!", call.=FALSE)
+
+  formula_terms <- terms(formula)
+  response <- as.character(attr(formula_terms, "variables"))[2]
+  predictors <- attr(formula_terms, "term.labels")
+  id_var <- NULL
+  for (term in predictors) {
+    if (grepl("as.factor", term)) {
+      id_var <- gsub("as.factor\\((.*)\\)", "\\1", term)
+      predictors <- predictors[predictors != term]
+      break
+    }
+  }
+  if (is.null(id_var)) {
+    for (term in predictors) {
+      if (is.factor(data[[term]])) {
+        id_var <- term
+        predictors <- predictors[predictors != id_var]
+        break
+      }
+    }
+  }
+
+  mf <- model.frame(formula, data)
+  Y <- model.response(mf)
+  Z <- model.matrix(reformulate(predictors), data)[, -1, drop = FALSE]
+  ID <- data[,id_var]
+
+  result <- linear_fe(Y, Z, ID, message)
+
+  return(result)
+}
+
+
+linear_fe.data <- function(data, Y.char, Z.char, ID.char, message) {
+  if (missing(data) || missing(Y.char) || missing(Z.char) || missing(ID.char))
+    stop("Arguments 'data', 'Y.char', 'Z.char', and 'ID.char' are all required!", call.=FALSE)
+
+  if (!all(c(Y.char, Z.char, ID.char) %in% colnames(data)))
+    stop("Some of the specified columns are not in the data!", call.=FALSE)
+
+  Y = data[, Y.char]
+  Z = data[, Z.char, drop = FALSE]
+  ID = data[, ID.char]
+
+  result <- linear_fe(Y, Z, ID, message)
+
+  return(result)
+}
+
+
 linear_fe.complete <- function(formula = NULL, data = NULL,
                                Y = NULL, Z = NULL, ID = NULL,
                                Y.char = NULL, Z.char = NULL, ID.char = NULL,
@@ -60,18 +205,18 @@ linear_fe.complete <- function(formula = NULL, data = NULL,
 
     data <- as.data.frame(cbind(Y, ID, Z))
     Y.char <- colnames(data)[1]
-    prov.char <- colnames(data)[2]
+    ID.char <- colnames(data)[2]
     Z.char <- colnames(Z)
-    data <- data[order(factor(data[,prov.char])),]
+    data <- data[order(factor(data[,ID.char])),]
 
-    n.prov <- sapply(split(data[, Y.char], data[, prov.char]), length)
+    n.prov <- sapply(split(data[, Y.char], data[, ID.char]), length)
     m <- length(n.prov) # number of providers
     n <- sum(n.prov) # number of observations
     p <- length(Z.char) # number of covariates
 
     Z <- as.matrix(data[,Z.char], drop = F)
     Y <- as.matrix(data[, Y.char, drop = F])
-    ID <- as.matrix(data[, prov.char, drop = F])
+    ID <- as.matrix(data[, ID.char, drop = F])
 
     Q <- lapply(n.prov, function(n) diag(n)-matrix(1, nrow = n, ncol = n)/n)
 
@@ -81,8 +226,8 @@ linear_fe.complete <- function(formula = NULL, data = NULL,
     colnames(beta) <- "beta"
     rownames(beta) <- Z.char
 
-    y_bar <- sapply(split(data[,Y.char], data[,prov.char]),mean)
-    Z_bar <- t(matrix(sapply(split(data[,Z.char, drop = FALSE], data[,prov.char]), colMeans),
+    y_bar <- sapply(split(data[,Y.char], data[,ID.char]),mean)
+    Z_bar <- t(matrix(sapply(split(data[,Z.char, drop = FALSE], data[,ID.char]), colMeans),
                       ncol=length(y_bar), nrow = length(beta)))
     gamma.prov <- as.matrix(y_bar - Z_bar %*% beta)
     colnames(gamma.prov) <- "gamma"
@@ -96,6 +241,7 @@ linear_fe.complete <- function(formula = NULL, data = NULL,
     linear_pred <- Z %*% beta
     gamma.obs <- rep(gamma.prov, n.prov)
     pred = gamma.obs + linear_pred
+    colnames(pred) <- "Prediction"
     residuals <- Y - pred
     sigma_hat_sq <- sum(residuals^2)/(n - m - p)
 
@@ -113,16 +259,29 @@ linear_fe.complete <- function(formula = NULL, data = NULL,
     variance$gamma <- var_gamma
 
     char_list <- list(Y.char = Y.char,
-                      prov.char = prov.char,
+                      ID.char = ID.char,
                       Z.char = Z.char)
 
   }
   else if (method == "lm") {
-    if (!is.null(formula) && !is.null(data)){
-      formula <- update(formula, . ~ . - 1)
+    if (!is.null(formula) && !is.null(data) && !is.null(ID.char)){
+      data[,ID.char] <- as.factor(data[,ID.char])
+      formula_terms <- all.vars(formula)
+      formula_terms <- formula_terms[formula_terms != ID.char]
+      Z.char <- formula_terms[2:length(formula_terms)]
+      Y.char <- formula_terms[1]
+      new_formula_terms <- c(formula_terms, ID.char)
+      # ID.char is always in the last position
+      new_formula <- as.formula(paste(new_formula_terms[1], "~",
+                                      paste(new_formula_terms[2:length(new_formula_terms)], collapse = " + ")))
+
+      data <- data[order(factor(data[,ID.char])),]
+      formula <- update(new_formula, . ~ . - 1)
       fit_lm <- lm(formula, data = data)
     }
     else if (!is.null(data) && !is.null(Y.char) && !is.null(Z.char) && !is.null(ID.char)) {
+      data[,ID.char] <- as.factor(data[,ID.char])
+      data <- data[order(factor(data[,ID.char])),]
       formula <- as.formula(paste(Y.char, "~", paste(Z.char, collapse = " + "), "+", ID.char, "-1"))
       fit_lm <- lm(formula, data = data)
     }
@@ -130,15 +289,67 @@ linear_fe.complete <- function(formula = NULL, data = NULL,
       if (length(Y) != length(ID) | (length(ID) != nrow(Z))) {
         stop("Dimensions of the input data do not match!!", call.=F)
       }
-      Y.char <- deparse(substitute(Y))
-      ID.char <- deparse(substitute(prov.ID))
+      data <- as.data.frame(cbind(Y, ID, Z))
+      Y.char <- colnames(data)[1]
+      ID.char <- colnames(data)[2]
+      Z.char <- colnames(Z)
+      data[,ID.char] <- as.factor(data[,ID.char])
+      data <- data[order(factor(data[,ID.char])),]
       formula <- as.formula(paste(Y.char, "~", paste(Z.char, collapse = " + "), "+", ID.char, "-1"))
       fit_lm <- lm(formula, data = data)
-
     }
     else {
       stop("Insufficient or incompatible arguments provided. Please provide either (1) formula and data, (2) data, Y.char, Z.char, and ID.char, or (3) Y, Z, and ID.", call.=FALSE)
     }
+
+    sum <- summary(fit_lm)
+
+    Z <- as.matrix(data[,Z.char], drop = F)
+    Y <- as.matrix(data[, Y.char, drop = F])
+    ID <- as.matrix(data[, ID.char, drop = F])
+    n.prov <- sapply(split(data[, Y.char], data[, ID.char]), length)
+    m <- length(n.prov) # number of providers
+    n <- sum(n.prov) # number of observations
+    p <- length(Z.char) # number of covariates
+
+    # Coefficients
+    beta <- matrix(sum$coefficients[1:p, 1], ncol = 1)
+    colnames(beta) <- "beta"
+    rownames(beta) <- Z.char
+    gamma.prov <- matrix(sum$coefficients[(p+1):(p+m), 1], ncol = 1)
+    colnames(gamma.prov) <- "gamma"
+    rownames(gamma.prov)<- names(n.prov)
+
+    coefficient <- list()
+    coefficient$beta <- beta
+    coefficient$gamma <- gamma.prov
+
+    # Variance
+    sigma_hat_sq <- sum$sigma^2
+    X <- model.matrix(fit_lm)
+    varcor <- sigma_hat_sq * solve(t(X)%*%X)
+    varcor_beta <- varcor[1:p, 1:p]
+    rownames(varcor_beta) <- Z.char
+    colnames(varcor_beta) <- Z.char
+
+    var_gamma <- matrix(diag(varcor)[(p+1):(p+m)], ncol = 1)
+    rownames(var_gamma) <- names(n.prov)
+    colnames(var_gamma) <- "Variance.Gamma"
+
+    variance <- list()
+    variance$beta <- varcor_beta
+    variance$gamma <- var_gamma
+
+    # Prediction
+    pred <- data[,Y.char] - sum$residuals
+    pred <- matrix(pred, ncol = 1)
+    colnames(pred) <- "Prediction"
+    rownames(pred) <- seq_len(nrow(pred))
+    linear_pred <- Z %*% beta
+
+    char_list <- list(Y.char = Y.char,
+                      ID.char = ID.char,
+                      Z.char = Z.char)
 
   }
   else stop("Method should be either 'pl' or 'lm'")
@@ -150,7 +361,7 @@ linear_fe.complete <- function(formula = NULL, data = NULL,
                            linear_pred = linear_pred,
                            prediction = pred,
                            observation = Y,
-                           prov = data[, prov.char]),
+                           prov = data[, ID.char]),
                       class = "linear_fe")  #define a list for prediction
 
   result$data_include <- data
